797be2c1b12064d018df5a3b72b955010a9a50a6
kolyasademetrio <kolyasademetrio@gmail.com>
2021-03-29 04:07:38 -0500

    homepage complete

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -74,4 +74,46 @@ $(document).ready(function () {
       showAutocompleteOnFocus: true,
       el: $('.keywords'),
     })
+
+    var word_arrays = [
+      {text: "#lost credit card", weight: 20, link: 'http://github.com/mistic100/jQCloud', html: {class: "category credit"}},
+      {text: "#credit limit", weight: 10.5, link: 'http://github.com/mistic100/jQCloud', html: {class: "category credit"}},
+      {text: "#news credit card", weight: 9.4, link: 'http://github.com/mistic100/jQCloud', html: {class: "category credit"}},
+      {text: "#credit card fees", weight: 8, link: 'http://github.com/mistic100/jQCloud', html: {class: "category credit"}},
+      {text: "#credit card stolen", weight: 7.5, link: 'http://github.com/mistic100/jQCloud', html: {class: "category credit"}},
+      {text: "#credit card", weight: 40, link: 'http://github.com/mistic100/jQCloud', html: {class: "category card"}},
+      {text: "#card", weight: 6.8, link: 'http://github.com/mistic100/jQCloud', html: {class: "category card"}},
+      {text: "#debit card", weight: 6.1, link: 'http://github.com/mistic100/jQCloud', html: {class: "category card"}},
+      {text: "#card charges", weight: 15, link: 'http://github.com/mistic100/jQCloud', html: {class: "category card"}},
+      {text: "#new card", weight: 3, link: 'http://github.com/mistic100/jQCloud', html: {class: "category card"}},
+      {text: "#avoid fees", weight: 30, link: 'http://github.com/mistic100/jQCloud', html: {class: "category fees"}},
+      {text: "#banking fees", weight: 2.8, link: 'http://github.com/mistic100/jQCloud', html: {class: "category fees"}},
+      {text: "#withdraw fees", weight: 1.75, link: 'http://github.com/mistic100/jQCloud', html: {class: "category fees"}},
+      {text: "#deposit fees", weight: 4, link: 'http://github.com/mistic100/jQCloud', html: {class: "category fees"}},
+      {text: "#card fees", weight: 16, link: 'http://github.com/mistic100/jQCloud', html: {class: "category fees"}},
+      {text: "#withdraw money", weight: 70, link: 'http://github.com/mistic100/jQCloud', html: {class: "category money"}},
+      {text: "#deposit money", weight: 1, link: 'http://github.com/mistic100/jQCloud', html: {class: "category money"}},
+      {text: "#lodge money", weight: 9, link: 'http://github.com/mistic100/jQCloud', html: {class: "category money"}},
+      {text: "#saving money", weight: 4, link: 'http://github.com/mistic100/jQCloud', html: {class: "category money"}},
+      {text: "#money security", weight: 12, link: 'http://github.com/mistic100/jQCloud', html: {class: "category money"}},
+      {text: "#atm withdraw", weight: 30, link: 'http://github.com/mistic100/jQCloud', html: {class: "category withdraw"}},
+      {text: "#withdraw money", weight: 7, link: 'http://github.com/mistic100/jQCloud', html: {class: "category withdraw"}},
+      {text: "#card card withdraw", weight: 3, link: 'http://github.com/mistic100/jQCloud', html: {class: "category withdraw"}},
+      {text: "#debit card withdraw", weight: 4, link: 'http://github.com/mistic100/jQCloud', html: {class: "category withdraw"}},
+      {text: "#withdraw", weight: 9.1, link: 'http://github.com/mistic100/jQCloud', html: {class: "category withdraw"}},
+    ];
+   //http://mistic100.github.io/jQCloud/demo.html
+  
+    $(".words-cloud").jQCloud(word_arrays, {
+      // width: 500,
+      height: 350,
+      autoResize: true,
+      afterCloudRender: function() {
+      //   $('.words-cloud > span').on('click', function(e){
+      //     e.preventDefault();
+      //     console.log(e.target.innerHTML);
+      //     $('.detailed-tags').prepend("<div class='tag'>" + e.target.innerHTML + "</div>")
+      //   });
+      }
+    });
 });
